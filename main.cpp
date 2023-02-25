e8f1f207bc36632a8024bbb5e9e709315e686b5b
Xenomorphing-XD <106723231+Xenomorphing-XD@users.noreply.github.com>
2023-02-25 12:22:04 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1, num2;
+
+  cout << "Enter first integer: ";
+  cin >> num1;
+
+  cout << "Enter second integer: ";
+  cin >> num2;
+
+  if (num1 > num2) {
+    cout << num1 << " is greater than " << num2 << endl;
+  } else if (num2 > num1) {
+    cout << num2 << " is greater than " << num1 << endl;
+  } else {
+    cout << num1 << " and " << num2 << " are equal" << endl;
+  }
+
+  return 0;
+}
